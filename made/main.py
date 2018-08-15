230ae8e0baee0aec62eb20bafc261a434719c5fb
sungwonlyu <lyusungwon@naver.com>
2018-08-15 17:18:00 +0900

    order

--- made/main.py
+++ made/main.py
@@ -115,7 +115,7 @@ def sample(inputs):
 	outputs = inputs.clone()
 	# sample = torch.randn(1, 1, args.input_h, args.input_w).to(device)
 	for i in range(args.start_sample, args.input_h * args.input_w):
-		samples = made(inputs)
+		samples = made(outputs)
 		# sample_add = torch.bernoulli(output.view(1, 1, args.input_h * args.input_w)* nmask).view(1, 1, args.input_h, args.input_w)
 		nmask = (mask == i).float().to(device)
 		# sample_add = torch.bernoulli(samples.view(len(inputs), 1, args.input_h * args.input_w)* nmask).view(len(inputs), 1, args.input_h, args.input_w)
